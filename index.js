3b5c0a1929062f757ae107b42634e36ba34fb030
barbapatrickhenry <barbapatrickhenry@gmail.com>
2023-12-04 19:37:53 +0800

    Deleted index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-index.js content:
-{
-    "capstone": {
-        "liveLink": "https://barbapatrickhenry.github.io/webportfolio/",
-        "name": "Patrick Henry Barba",
-        "isMulti": false
-    }
-}
-
